9b510622d3d14c1778941b013ba2432a71d4d2d2
zonaetmunna <zonaetmonna@gmail.com>
2022-05-12 21:03:25 +0600

    mongodb add

--- app.js
+++ app.js
@@ -3,12 +3,13 @@
 const express=require('express');
 const cors=require('cors');
 require('dotenv').config();
+const mongodb=require('mongodb');
 
 // app declared
 const app=express();
 
 // port declared
-const port=process.env.PORT| 5000;
+const port=process.env.PORT | 5000;
 
 // build in  middleware
 app.use(express.json());
@@ -21,6 +22,17 @@ app.get('/',(req,res)=>{
 });
 
 
+
+
+// default error handler
+const errorHandler=(err,req,res,next)=>{
+     if(res.headersSent){
+          return next(err);
+     }else{
+          res.status(5000).json({error:err});
+     }
+
+};
 
 // listen app
 app.listen(port,()=>{
